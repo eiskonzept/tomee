b0d117635edcb3b0b7ec01fef758f247da4082b5
eiskonzept <dmitry.volkov@eiskonzept.com>
2013-02-20 19:15:39 +0100

    Serialize deserealize Exception

--- src/main/java/de/test/PassivationCapableBean.java
+++ src/main/java/de/test/PassivationCapableBean.java
@@ -1,11 +1,17 @@
 package de.test;
 
+import org.apache.commons.lang.SerializationUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.WindowScoped;
+import org.apache.myfaces.extensions.cdi.core.impl.projectstage.ProjectStageProducer;
+import org.apache.myfaces.extensions.cdi.jsf.impl.scope.conversation.spi.EditableWindowContext;
+import org.apache.myfaces.extensions.cdi.jsf2.impl.request.DefaultRequestTypeResolver;
 
 import javax.ejb.EJB;
 import javax.inject.Named;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 @Named
@@ -38,7 +44,16 @@ public class PassivationCapableBean implements Serializable {
     }
 
     public String submit(){
-        log.error("Test!");
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        SerializationUtils.serialize(new DefaultRequestTypeResolver(), byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        try {
+            byteArrayOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        Object deserialize = SerializationUtils.deserialize(bytes);
+        System.out.println(deserialize);
         return null;
     }
 }
